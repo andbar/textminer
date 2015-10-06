d46a7489dac40faca8593d7f684f5ffd94d30476
andbar <barringera@gmail.com>
2015-10-06 13:49:56 -0400

    words and phone_number done

--- textminer/separator.py
+++ textminer/separator.py
@@ -2,4 +2,16 @@ import re
 
 #take a string and return list of the words in the string; exclude numbers only
 def words(string_in):
-    pass
+    match = re.findall(r"\d*\S*[A-Za-z]\w+", string_in)
+    if match == []:
+        return None
+    else:
+        return match
+
+def phone_number(num_string):
+    match = re.search(r"\(?(\d{3})\)?[-.]?\s*(\d{3})[\-\.]?(\d{4})", num_string)
+    if match == None:
+        return None
+    else:
+        cleaned = "{}-{}-{}".format(*match.groups())
+        return {"area_code": cleaned[0:3], "number": cleaned[4:12]}
